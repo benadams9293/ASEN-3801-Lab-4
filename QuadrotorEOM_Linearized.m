9bbc39b91d6fbdece21728dc64ea4fd4d55eb0b4
BuffBotBrawlers <BuffBotBrawlers@colorado.edu>
2026-03-03 16:03:51 -0700

    Create QuadrotorEOM_Linearized.m

---
+++ QuadrotorEOM_Linearized.m
@@ -0,0 +1,22 @@
+%{
+Task: 2.2
+Contributor(s): Ben Adams
+Course number: ASEN 3801
+File name: QuadrotorEOM_Linearized.m
+Created: 03/03/2026
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+- Goal: 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+- Methodology: 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+%}
+
+function var_dot = QuadrotorEOM_Linearized(t, var, g, m, I, deltaFc, deltaGc)
+%{
+Inputs:
+var = 12x1 column matrix
+deltaFc = deviation from the steady hover trim condition
+deltaGc = deviation from the steady hover trim condition
+%}
+
+end
