05892cde6062208078baacf360248cf7fddbe2f5
BuffBotBrawlers <BuffBotBrawlers@colorado.edu>
2026-03-03 16:17:09 -0700

    Create QuadrotorEOMwithRateFeedback.m

---
+++ QuadrotorEOMwithRateFeedback.m
@@ -0,0 +1,18 @@
+%{
+Task: 2.4
+Contributor(s): Ben Adams
+Course number: ASEN 3801
+File name: ComputeMotorForces.m
+Created: 03/03/2026
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+- Goal: linearized model of the full quadrotor dynamics
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+- Methodology: 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+Inputs:
+
+Outputs:
+
+%}
+function var_dot = QuadrotorEOMwithRateFeedback(t, var, g, m, I, nu, mu)
+end
