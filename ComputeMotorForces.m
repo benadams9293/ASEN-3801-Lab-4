6d250d62ce6a82fe47df307ec0d2bd37a348e07c
BuffBotBrawlers <BuffBotBrawlers@colorado.edu>
2026-03-03 16:14:53 -0700

    headers added

--- ComputeMotorForces.m
+++ ComputeMotorForces.m
@@ -1,11 +1,22 @@
 %{
+Task: 2.4
 Contributor(s): Ben Adams
 Course number: ASEN 3801
-File name: Lab4_main.m
+File name: ComputeMotorForces.m
 Created: 03/03/2026
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 - Goal: linearized model of the full quadrotor dynamics
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 - Methodology: 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+Inputs:
+d = d is the distance from aircraft CG to each rotor
+km = control moment coefficient
+
+Outputs:
+motor_forces = 4x1 column vector
+
 %}
+function motor_forces = ComputeMotorForces(Fc, Gc, d, km)
+
+end
