450af23c481fd01b556768a08d0370a3b78bff75
jacobonthecorn <jale9668@colorado.edu>
2026-03-10 15:52:40 -0600

    Add files via upload

--- main12.m
+++ main12.m
@@ -6,10 +6,11 @@ const = getConst();
 % Initial Conditions
 x0 = 0;
 y0 = 0;
-z0 = -900;
+z0 = 0;
 phi0 = 0;
+phi1 = deg2rad(5);
 theta0 = 0;
-psi0 = 120;
+psi0 = 0;
 u0 = 0;
 v0 = 0;
 w0 = 0;
@@ -19,7 +20,8 @@ r0 = 0;
 
 
 % Define the initial state vector
-initialState = [x0;y0;z0;phi0;theta0;psi0;u0;v0;w0;p0;q0;r0];
+initialState1 = [x0;y0;z0;phi0;theta0;psi0;u0;v0;w0;p0;q0;r0];
+initialState2 = [x0;y0;z0;phi1;theta0;psi0;u0;v0;w0;p0;q0;r0];
 I = [const.I_x, 0, 0; 0, const.I_y, 0; 0, 0, const.I_z];
 force = const.m*const.g/4;
 motor_forces = [force;force;force;force]; % Placeholder for motor forces
@@ -27,8 +29,13 @@ motor_forces = [force;force;force;force]; % Placeholder for motor forces
 tFinal = 10; % seconds
 dt = 0.01;
 tSpan = [0 tFinal];
-t = 0:dt:tFinal;
-[t, aircraft_state_array] = ode45(@(t,var) QuadrotorEOM(t, var, const.g, const.m, I, const.d, const.km, const.nu, const.mu, motor_forces), tSpan, initialState);
+control_input_array = [motor_forces; 0; 0; 0];
+
+[t1, aircraft_state_array1] = ode45(@(t,var) QuadrotorEOM(t, var, const.g, const.m, I, const.d, const.km, const.nu, const.mu, motor_forces), tSpan, initialState1);
+[t2, aircraft_state_array2] = ode45(@(t,var) QuadrotorEOM(t, var, const.g, const.m, I, const.d, const.km, const.nu, const.mu, motor_forces), tSpan, initialState2);
+
+PlotAircraftSim(t1, aircraft_state_array1', control_input_array, [1 2 3 4 5 6], 'b-')
+PlotAircraftSim(t2, aircraft_state_array2', control_input_array, [1 2 3 4 5 6], 'r-')
 
 
 function const = getConst()
