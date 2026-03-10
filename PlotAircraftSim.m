c42f0716e1aa2c86d96fb37eee5c566ffcdaec7a
JeffWik1 <jeffrey.wik@colorado.edu>
2026-03-10 15:52:12 -0600

    Add files via upload

--- PlotAircraftSim.m
+++ PlotAircraftSim.m
@@ -1,6 +1,6 @@
 %{
 Task: 1.1
-Contributor(s): Jacob Legg, Jeremy Li, Jeff Wik, Jay Berlin
+Contributor(s): Ben Adams, Jacob Legg, Jeremy Li, Jeff Wik, Jay Berlin
 Course number: ASEN 3801
 File name: PlotAircraftSim.m
 Created: 03/03/2026
@@ -13,8 +13,8 @@ Created: 03/03/2026
 Inputs:
 aircraft_state_array = 12xn a/c state vector
 control_input_array = 4xn control input state vector
-fig = 
-col = 
+fig = 6x1 vector of figure numbers to plot over
+col = plotting option used for every plot
 
 Outputs:
 Plots of...
@@ -34,76 +34,77 @@ fig = fig(:);
 figure(fig(1))
 subplot(3,1,1)
 plot(time, aircraft_state_array(1,:), col); hold on
-xlabel('Time (s)'); ylabel('x_E'); title('Inertial Position (x component)');
+xlabel('Time (s)'); ylabel('x_E (m)'); title('Inertial Position (x component)');
 
 subplot(3,1,2)
 plot(time, aircraft_state_array(2,:), col); hold on
-xlabel('Time (s)'); ylabel('y_E'); title('Inertial Position (y component)');
+xlabel('Time (s)'); ylabel('y_E (m)'); title('Inertial Position (y component)');
 
 subplot(3,1,3)
 plot(time, aircraft_state_array(3,:), col); hold on
-xlabel('Time (s)'); ylabel('z_E'); title('Inertial Position (z component)');
-
+xlabel('Time (s)'); ylabel('z_E (m)'); title('Inertial Position (z component)');
+%saveas('Position_Plot.jpg')
+%---------------------------------------------
 % Plot Attitude
 figure(fig(2))
 subplot(3,1,1)
 plot(time, aircraft_state_array(4,:), col); hold on
-xlabel('Time (s)'); ylabel('phi'); title('Attitude');
+xlabel('Time (s)'); ylabel('psi (rads)'); title('Attitude');
 
 subplot(3,1,2)
 plot(time, aircraft_state_array(5,:), col); hold on
-xlabel('Time (s)'); ylabel('theta'); title('Attitude');
+xlabel('Time (s)'); ylabel('theta (rads)'); title('Attitude');
 
 subplot(3,1,3)
 plot(time, aircraft_state_array(6,:), col); hold on
-xlabel('Time (s)'); ylabel('psi'); title('Attitude');
-
+xlabel('Time (s)'); ylabel('phi (rads)'); title('Attitude');
+%---------------------------------------------
 % Plot Inertial Velocity
 figure(fig(3))
 subplot(3,1,1)
 plot(time, aircraft_state_array(7,:), col); hold on
-xlabel('Time (s)'); ylabel('u^E'); title('Inertial Velocity (x component)');
+xlabel('Time (s)'); ylabel('u^E (m/s)'); title('Inertial Velocity (x component)');
 
 subplot(3,1,2)
 plot(time, aircraft_state_array(8,:), col); hold on
-xlabel('Time (s)'); ylabel('v^E'); title('Inertial Velocity (y component)');
+xlabel('Time (s)'); ylabel('v^E (m/s)'); title('Inertial Velocity (y component)');
 
 subplot(3,1,3)
 plot(time, aircraft_state_array(9,:), col); hold on
-xlabel('Time (s)'); ylabel('w^E'); title('Inertial Velocity (z component)');
-
+xlabel('Time (s)'); ylabel('w^E (m/s)'); title('Inertial Velocity (z component)');
+%---------------------------------------------
 % Plot Angular Velocity
 figure(fig(4))
 subplot(3,1,1)
 plot(time, aircraft_state_array(10,:), col); hold on
-xlabel('Time (s)'); ylabel('p'); title('Angular Velocity (x component)');
+xlabel('Time (s)'); ylabel('p (rads/s)'); title('Angular Velocity (x component)');
 
 subplot(3,1,2)
 plot(time, aircraft_state_array(11,:), col); hold on
-xlabel('Time (s)'); ylabel('q'); title('Angular Velocity (y component)');
+xlabel('Time (s)'); ylabel('q (rads/s)'); title('Angular Velocity (y component)');
 
 subplot(3,1,3)
 plot(time, aircraft_state_array(12,:), col); hold on
-xlabel('Time (s)'); ylabel('r'); title('Angular Velocity (z component)');
-
+xlabel('Time (s)'); ylabel('r (rads/s)'); title('Angular Velocity (z component)');
+%---------------------------------------------
 % Plot Control Inputs
 figure(fig(5))
 subplot(4,1,1)
 plot(time, control_input_array(1,:), col); hold on
-xlabel('Time (s)'); ylabel('Z_c'); title('Control Force (z direction)');
+xlabel('Time (s)'); ylabel('Z_c (N)'); title('Control Force (z direction)');
 
 subplot(4,1,2)
 plot(time, control_input_array(2,:), col); hold on
-xlabel('Time (s)'); ylabel('L_c'); title('Control Roll Moment');
+xlabel('Time (s)'); ylabel('L_c (Nm)'); title('Control Roll Moment');
 
 subplot(4,1,3)
 plot(time, control_input_array(3,:), col); hold on
-xlabel('Time (s)'); ylabel('M_c'); title('Control Pitching Moment');
+xlabel('Time (s)'); ylabel('M_c (Nm)'); title('Control Pitching Moment');
 
 subplot(4,1,4)
 plot(time, control_input_array(4,:), col); hold on
-xlabel('Time (s)'); ylabel('N_c'); title('Control Yaw Moment');
-
+xlabel('Time (s)'); ylabel('N_c (Nm)'); title('Control Yaw Moment');
+%---------------------------------------------
 % Plot Flight Path
 figure(fig(6))
 plot3(aircraft_state_array(1,:), aircraft_state_array(2,:), aircraft_state_array(3,:), col); hold on;
@@ -111,4 +112,6 @@ xlabel('x-pos (m)'); ylabel('y-pos (m)'); zlabel('z-pos (m)'); title('3D Aircraf
     % Start / finish markers
     plot3(aircraft_state_array(1,1),  aircraft_state_array(2,1),  aircraft_state_array(3,1),  'go', 'MarkerFaceColor','g'); hold on;
     plot3(aircraft_state_array(1,end),aircraft_state_array(2,end),aircraft_state_array(3,end),'ro', 'MarkerFaceColor','r'); hold on;
+
 end
+
