76c946add4780c5216cd600b400b81e05927eaf8
BuffBotBrawlers <BuffBotBrawlers@colorado.edu>
2026-03-10 15:11:18 -0600

    task 2 started

--- Lab4_main.m
+++ Lab4_main.m
@@ -1,5 +1,5 @@
 %{
-Contributor(s): 
+Contributor(s): Ben Adams
 Course number: ASEN 3801
 File name: Lab4_main.m
 Created: 03/03/2026
@@ -10,18 +10,22 @@ Created: 03/03/2026
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
 %}
 
-%% Task 2.1
-nonlinear_IC = ...
+%% Task 2.1 
+nonlinear_IC_changes = ...
    [0 0 0 0 0 0;
     0 0 0 0 0 0;
     0 0 0 0 0 0;
     deg2rad(5) 0 0 0 0 0; %roll
     0 deg2rad(5) 0 0 0; %pitch
-    0 0 0 0 deg2rad(5) 0; %yaw
+    0 0 deg2rad(5) 0 0 0; %yaw
     0 0 0 0 0 0;
     0 0 0 0 0 0;
     0 0 0 0 0 0;
-    0 0 0 0 0 0;
-    0 0 0 0 0 0;
-    0 0 0 0 0 0];
-x_dot = QuadrotorEOM();
+    0 0 0 .1 0 0; %roll rate
+    0 0 0 0 .1 0; %pitch rate
+    0 0 0 0 0 .1]; %yaw rate
+nonlinear_IC = zeros(12,1);
+for i = 1:6
+    x_dot = QuadrotorEOM();
+    PlotAircraftSim()
+end
