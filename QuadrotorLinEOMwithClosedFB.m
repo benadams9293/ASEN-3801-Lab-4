2142106af9b34d5901520a0089ec095600acd99c
jacobonthecorn <jale9668@colorado.edu>
2026-03-24 16:41:09 -0600

    Brief Changes

--- QuadrotorLinEOMwithClosedFB.m
+++ QuadrotorLinEOMwithClosedFB.m
@@ -14,7 +14,9 @@ Created: 03/24/2026
 function var_dot = QuadrotorLinEOMwithClosedFB(t, var, g, m, I)
     % 1. Get commanded control forces and moments  (3.2)
     [Fc, Gc] = InnerLoopFeedback(var, m, g);
+
+    delta_Fc = zeros(3,1);
     
     % 2. Feed control moments and forces linear EOM (2.2)
-    var_dot = QuadrotorEOM_Linearized(t, var, g, m, I, Fc, Gc);
+    var_dot = QuadrotorEOM_Linearized(t, var, g, m, I, delta_Fc, Gc);
 end
