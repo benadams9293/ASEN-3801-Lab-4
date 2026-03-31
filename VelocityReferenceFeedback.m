84b32dd98c44413370ea56bc8ce4deea31c7a928
BuffBotBrawlers <BuffBotBrawlers@colorado.edu>
2026-03-31 15:39:26 -0600

    Create VelocityReferenceFeedback.m

---
+++ VelocityReferenceFeedback.m
@@ -0,0 +1,36 @@
+function [Fc, Gc] = VelocityReferenceFeedback(var, m, g, t)
+%{
+Task: 3.2
+Contributor(s): Ben Adams, Jacob Legg, Jeremy Li, Jeff Wik, Jay Berlin
+Course number: ASEN 3801
+File name: VelocityReferenceFeedback.m
+Created: 03/31/2026
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+- Goal: Create a function to calculate the control vectors Fc and Gc,
+Includes K_3
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+- Methodology: 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+Inputs:
+var = 12xn a/c state vector
+m = aircraft mass
+g = gravitational acceleration
+
+Outputs:
+Fc = 3x1 Control Forces (body z-component only)
+Gc = 3x1 body Control Moments
+
+%}
+phi = var(4);
+theta = var(5);
+p = var(10);
+q = var(11);
+r = var(12);
+
+Fc = [0; 0; -m*g];
+
+Gc = [-3.98*10^-4*p-5.63*10^-4*phi;
+      -3.98*10^-4*q-5.63*10^-4*theta;
+      -0.004*r];
+end
+
