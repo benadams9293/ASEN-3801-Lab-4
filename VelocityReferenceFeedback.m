75d75fa9ddcba5ec5b5303ad260bf3611ed39324
JeremyL <114038490+Jl2580@users.noreply.github.com>
2026-03-31 16:41:21 -0600

    Add files via upload

--- VelocityReferenceFeedback.m
+++ VelocityReferenceFeedback.m
@@ -1,36 +1,58 @@
-function [Fc, Gc] = VelocityReferenceFeedback(var, m, g, t)
 %{
-Task: 3.6
-Contributor(s): Ben Adams, Jacob Legg, Jeremy Li, Jeff Wik, Jay Berlin
+Contributor(s): Jeremy Li
 Course number: ASEN 3801
-File name: VelocityReferenceFeedback.m
-Created: 03/31/2026
+File name: L3801Lab4_14
+Created: 03/10/2026
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-- Goal: Create a function to calculate the control vectors Fc and Gc,
-Includes K_3
+- Goal: 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 - Methodology: 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-Inputs:
-var = 12xn a/c state vector
-m = aircraft mass
-g = gravitational acceleration
-
-Outputs:
-Fc = 3x1 Control Forces (body z-component only)
-Gc = 3x1 body Control Moments
-
 %}
-phi = var(4);
-theta = var(5);
-p = var(10);
-q = var(11);
-r = var(12);
 
-Fc = [0; 0; -m*g];
 
-Gc = [-3.98*10^-4*p-5.63*10^-4*phi;
-      -3.98*10^-4*q-5.63*10^-4*theta;
-      -0.004*r];
+
+function [Fc, Gc] = VelocityReferenceFeedback(t, var)
+    m = 0.068;
+    g = 9.81;
+    
+    % HARDCODE GAINS
+    %Ix=lat, Iy=lon
+    % Lateral (Roll/Sway) Gains from 3.1 and 3.5
+    k1_lat = 3.98e-4; 
+    k2_lat = 5.63e-4; 
+    k3_lat = 2.306e-5; 
+    
+    % Longitudinal (Pitch/Surge) Gains from 3.1 and 3.5
+    k1_lon = 4.94e-4; 
+    k2_lon = 6.99e-4; 
+    k3_lon = 2.856e-5; % Check sign
+    
+    k_yaw = 0.004;
+    
+    phi = var(4); 
+    theta = var(5);
+    u = var(7); 
+    v = var(8);
+    p = var(10); 
+    q = var(11); 
+    r = var(12);
+    
+    if t <= 2.0
+        u_r = 0.5;
+        v_r = 0.5;
+    else
+        u_r = 0.0;
+        v_r = 0.0;
+    end
+    
+    Fc = [0; 0; -m*g]; % Control Forces
+    
+    Lc = -k1_lat*p - k2_lat*phi + k3_lat*(v_r-v); % Control Moments
+    
+    Mc = -k1_lon*q - k2_lon*theta - k3_lon*(u_r-u); % Longitudinal Control
+    
+    Nc = -k_yaw*r; % Yaw Control (from 2.3)
+    
+    Gc = [Lc; Mc; Nc]; % Combine
 end
-
