b36c6a3e35785c2f69b8dff108a542b511a1b97e
jacobonthecorn <jale9668@colorado.edu>
2026-03-31 16:46:27 -0600

    3.5 again

--- Lab4_main.m
+++ Lab4_main.m
@@ -87,8 +87,8 @@ k3 = linspace(1*10^-4,1*10^-5,1000);
 for i=1:1000
     A_x = [0,9.81,0;0,0,1;-k3(i)/I_x,-k2x/I_x,-k1x/I_x];
     A_y = [0,9.81,0;0,0,1;-k3(i)/I_y,-k2y/I_y,-k1y/I_y];
-    % [~,D] = eig(A_x);
-    [~,D] = eig(A_y);
+    [~,D] = eig(A_x);
+    % [~,D] = eig(A_y);
     % Extract eigenvalues for further analysis
     eigenvalue1(i) = D(1,1);
     eigenvalue2(i) = D(2,2);
@@ -100,7 +100,7 @@ hold on;
 plot(eigenvalue2)
 plot(eigenvalue3)
 legend('2','3')
-chosen = 1000 - length(find(abs(eigenvalue2)<=0.8));
+chosen = length(eigenvalue2) - length(find(abs(eigenvalue2)<=0.8));
 % Store the eigenvalue corresponding to the chosen index for further analysis
 selectedk3 = k3(chosen);
 selectedTau = -1/eigenvalue2(chosen);
